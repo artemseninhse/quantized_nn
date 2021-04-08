228a4fc6fdcf54b99676930468359cd21f26beb6
Artem <ASSenin@sberbank.ru>
2021-04-08 22:21:40 +0300

    dataset_name argument added to unit_tests

--- unit_tests.py
+++ unit_tests.py
@@ -7,9 +7,11 @@ from utils import CROP_SIZE
 
 
 @test_verbose
-def check_loader_output(batch_size, crop_size):
+def check_loader_output(dataset_name,
+                        batch_size,
+                        crop_size):
     loader = dataloader.create_dataloader(
-        "mnist",
+        dataset_name,
         "test",
         batch_size
     )
@@ -21,12 +23,17 @@ def check_loader_output(batch_size, crop_size):
 
 
 parser = ArgumentParser()
+parser.add_argument("--dataset_name",
+                    default="mnist",
+                    required=False)
 parser.add_argument("--batch_size",
                     default=8,
                     required=False)
 
 if __name__ == "__main__":
     args = parser.parse_args()
+    dataset_name = args.dataset_name
     batch_size = int(args.batch_size)
-    check_loader_output(batch_size,
+    check_loader_output(dataset_name,
+                        batch_size,
                         CROP_SIZE)
